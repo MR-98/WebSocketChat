2ee80ad45cb553aa59f737bda217532b8a9684a4
Mateus Rosiak <rosa100@op.pl>
2025-01-05 00:24:07 +0100

    Configure websockets, add websocket controller

---
+++ backend/src/main/kotlin/com/mr/websocket_chat/controller/ChatController.kt
@@ -0,0 +1,29 @@
+package com.mr.websocket_chat.controller
+
+import com.mr.websocket_chat.domain.ChatMessage
+import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.stereotype.Controller
+
+
+@Controller
+class ChatController {
+
+	@Autowired
+	private lateinit var messagingTemplate: SimpMessagingTemplate
+
+	companion object {
+		private val LOG = KotlinLogging.logger{}
+	}
+
+	@MessageMapping("/chat.sendMessage")
+	fun sendMessage(@Payload message: ChatMessage) {
+		LOG.warn { "RECEIVED MESSAGE: " + message.data + " FROM: " + message.sender}
+		//TODO: checking if user is room member
+		messagingTemplate.convertAndSend("/topic/" + message.room, message)
+	}
+
+}
---
+++ backend/src/main/kotlin/com/mr/websocket_chat/domain/ChatMessage.kt
@@ -0,0 +1,7 @@
+package com.mr.websocket_chat.domain
+
+class ChatMessage(
+	val data: String,
+	val room: String,
+	val sender: String,
+)
