cb98350572d92961d62916e6e385708b2d3ca6b7
Mateus Rosiak <rosa100@op.pl>
2025-01-14 19:24:38 +0100

    app.component.ts clean up

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,61 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WebSocketService } from "./service/web-socket-service.service";
-import { FormsModule } from "@angular/forms";
-import { NgForOf, NgIf } from "@angular/common";
-import { ChatMessage } from "./model/chat-message";
-import { HeaderComponent } from "./component/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgIf, NgForOf, HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
-  roomId: number = 1;
-  message: string = '';
-  messages: ChatMessage[] = [];
-  connectedRoom: number | null = null;
-
-  constructor(
-    private webSocketService: WebSocketService
-  ) {}
-
-  connectToRoom(): void {
-    if (!this.roomId) {
-      alert('Room name is required');
-      return;
-    }
-
-    this.webSocketService.connect(
-      () => {
-        this.webSocketService.subscribeToRoom(
-          this.roomId,
-          (message: ChatMessage[] | ChatMessage) => {
-            if(Array.isArray(message)) {
-              this.messages.push(...message);
-            } else {
-              this.messages.push(message);
-            }
-          }
-        );
-      }
-    );
-
-    this.connectedRoom = this.roomId;
-  }
-
-  sendMessage(): void {
-    if (this.connectedRoom && this.message) {
-      this.webSocketService.sendMessage(this.connectedRoom, this.message);
-      this.message = '';
-    }
-  }
-
-  logout(): void {
-    this.webSocketService.disconnect();
-  }
 }
