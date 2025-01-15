c647fafd7b714b2ac112c271baf692d56ef8ee39
Mateus Rosiak <rosa100@op.pl>
2025-01-15 18:24:14 +0100

    Fix http interceptors not adding bearer token to headers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { KeycloakBearerInterceptor, KeycloakService } from "keycloak-angular";
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
     {
       provide: APP_INITIALIZER,
       useFactory: initializeKeycloak,
