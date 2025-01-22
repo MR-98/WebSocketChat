3d0fd267dd3e753c03bf5e4854f6aa370efa46c8
Mateus Rosiak <rosa100@op.pl>
2025-01-22 19:34:10 +0100

    Displaying the newest message below chat room name in chat room list element component added

--- frontend/src/app/component/chat-room-list-element/chat-room-list-element.component.ts
+++ frontend/src/app/component/chat-room-list-element/chat-room-list-element.component.ts
@@ -1,28 +1,60 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ChatRoom } from "../../model/chat-room";
 import { DataStoreService } from "../../service/data-store.service";
-import { NgClass } from "@angular/common";
+import { NgClass, NgIf } from "@angular/common";
+import { WebSocketService } from "../../service/web-socket.service";
+import { ChatMessage } from "../../model/chat-message";
+import { StompSubscription } from "@stomp/stompjs";
 
 @Component({
   selector: 'app-chat-room-list-element',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    NgIf
   ],
   templateUrl: './chat-room-list-element.component.html',
   styleUrl: './chat-room-list-element.component.scss'
 })
-export class ChatRoomListElementComponent {
+export class ChatRoomListElementComponent implements OnInit, OnDestroy{
+
   @Input() chatRoom: ChatRoom = {
     name: "",
     users: [],
     id: 0
   }
 
-  constructor(private dataStoreService: DataStoreService) {
+  protected lastMessage: ChatMessage | undefined;
+  private currentSubscription: StompSubscription | undefined;
+
+  constructor(
+    private dataStoreService: DataStoreService,
+    private webSocketService: WebSocketService
+  ) {}
+
+  ngOnInit() {
+    this.currentSubscription = this.webSocketService.subscribeToRoom(
+      this.chatRoom.id!!,
+      (message: ChatMessage[] | ChatMessage) => {
+        if (Array.isArray(message) && message.length > 0) {
+          this.lastMessage = <ChatMessage>(message.pop());
+        } else {
+          this.lastMessage = <ChatMessage>message;
+        }
+      }
+    )
   }
 
   isCurrentlySelectedChatRoom() {
     return this.dataStoreService.getCurrentlySelectedChatRoom()?.id == this.chatRoom.id
+  }
+
+  getLastMessageText() {
+    if(this.lastMessage?.sender == undefined|| this.lastMessage?.data == undefined) return ""
+    return this.lastMessage?.sender + ": " + this.lastMessage?.data;
+  }
+
+  ngOnDestroy() {
+    this.currentSubscription?.unsubscribe();
   }
 }
