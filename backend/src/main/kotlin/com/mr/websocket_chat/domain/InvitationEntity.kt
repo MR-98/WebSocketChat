6b8f71ecf6d759d23a061608d101e4783fe54976
Mateus Rosiak <rosa100@op.pl>
2025-01-25 14:13:11 +0100

    Add invitingUser to InvitationEntity. Adjust frontend model

--- backend/src/main/kotlin/com/mr/websocket_chat/domain/InvitationEntity.kt
+++ backend/src/main/kotlin/com/mr/websocket_chat/domain/InvitationEntity.kt
@@ -11,6 +11,9 @@ class InvitationEntity (
 	@ManyToOne
 	@JoinColumn(name = "invited_user")
 	val invitedUser: UserEntity,
+	@ManyToOne
+	@JoinColumn(name = "inviting_user")
+	val invitingUser: UserEntity,
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	val id: Long = 0,
