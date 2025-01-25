13e056df78b038ab729f6c7dc3649a516a73da96
Mateus Rosiak <rosa100@op.pl>
2025-01-25 14:13:42 +0100

    Backend for accepting or rejecting invitations implemented

--- backend/src/main/kotlin/com/mr/websocket_chat/repository/InvitationRepository.kt
+++ backend/src/main/kotlin/com/mr/websocket_chat/repository/InvitationRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface InvitationRepository : JpaRepository<InvitationEntity, Long> {
 	fun getAllByInvitedUser_Username(username: String, limit: Limit): List<InvitationEntity>
+	fun findByRoom_IdAndInvitedUser_Username(roomId: Long, username: String): InvitationEntity?
 }
--- backend/src/main/kotlin/com/mr/websocket_chat/service/InvitationService.kt
+++ backend/src/main/kotlin/com/mr/websocket_chat/service/InvitationService.kt
@@ -1,6 +1,8 @@
 package com.mr.websocket_chat.service
 
+import com.mr.websocket_chat.domain.ChatRoomEntity
 import com.mr.websocket_chat.domain.InvitationEntity
+import com.mr.websocket_chat.repository.ChatRoomRepository
 import com.mr.websocket_chat.repository.InvitationRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -8,7 +10,8 @@ import org.springframework.data.domain.Limit
 
 @Service
 class InvitationService @Autowired constructor(
-	private val invitationRepository: InvitationRepository
+	private val invitationRepository: InvitationRepository,
+	private val roomRepository: ChatRoomRepository
 ){
 	fun saveInvitation(invitation: InvitationEntity) {
 		invitationRepository.save(invitation)
@@ -16,6 +19,21 @@ class InvitationService @Autowired constructor(
 
 	fun loadInvitationsForUser(username: String): List<InvitationEntity> {
 		return invitationRepository.getAllByInvitedUser_Username(username, Limit.of(50))
+	}
+
+	fun findInvitationForRoomAndUser(roomId: Long, username: String): InvitationEntity? {
+		return invitationRepository.findByRoom_IdAndInvitedUser_Username(roomId, username)
+	}
+
+	fun acceptInvitation(invitation: InvitationEntity): ChatRoomEntity {
+		invitation.room.users.add(invitation.invitedUser)
+		val room = this.roomRepository.save(invitation.room)
+		deleteInvitation(invitation)
+		return room
+	}
+
+	fun deleteInvitation(invitation: InvitationEntity) {
+		this.invitationRepository.deleteById(invitation.id)
 	}
 
 }
