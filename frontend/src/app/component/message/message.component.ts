3d4ca177a503f07f913ab0a0d54cc0ead7545965
Mateus Rosiak <rosa100@op.pl>
2025-01-25 18:26:05 +0100

    Displaying message's timestamp in mat-tooltip added

--- frontend/src/app/component/message/message.component.ts
+++ frontend/src/app/component/message/message.component.ts
@@ -2,13 +2,15 @@ import { Component, Input } from '@angular/core';
 import { ChatMessage } from "../../model/chat-message";
 import { NgClass, NgIf } from "@angular/common";
 import { DataStoreService } from "../../service/data-store.service";
+import { MatTooltip } from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-message',
   standalone: true,
   imports: [
     NgClass,
-    NgIf
+    NgIf,
+    MatTooltip
   ],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
@@ -22,4 +24,8 @@ export class MessageComponent{
     this.currentUser = dataStoreService.getUserProfile()!!.username;
   }
 
+  formatDate(timestamp: number): string {
+    let date = new Date(timestamp);
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+  }
 }
