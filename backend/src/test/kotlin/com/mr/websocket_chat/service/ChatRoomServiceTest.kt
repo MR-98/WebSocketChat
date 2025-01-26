e9bbf85f4a5cac4611ee7a34769deceab86061bd
Mateus Rosiak <rosa100@op.pl>
2025-01-26 16:46:53 +0100

    Tests

---
+++ backend/src/test/kotlin/com/mr/websocket_chat/service/ChatRoomServiceTest.kt
@@ -0,0 +1,43 @@
+package com.mr.websocket_chat.service
+
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+
+class ChatRoomServiceTest {
+
+	@BeforeEach
+	fun setUp() {
+	}
+
+	@Test
+	fun addChatRoom() {
+	}
+
+	@Test
+	fun findById() {
+	}
+
+	@Test
+	fun getChatRoomsForUser() {
+	}
+
+	@Test
+	fun removeUserFromRoom() {
+	}
+
+	@Test
+	fun chatRoomExists() {
+	}
+
+	@Test
+	fun deleteChatRoom() {
+	}
+
+	@Test
+	fun isUserChatRoomMember() {
+	}
+
+	@Test
+	fun testIsUserChatRoomMember() {
+	}
+}
