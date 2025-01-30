c6d28df7aae3e5debd47d87fd248fff3331953bd
Mateus Rosiak <rosa100@op.pl>
2025-01-30 17:38:24 +0100

    [#1] Frontend for loading old messages implemented

--- frontend/src/app/component/chat-room/chat-room.component.ts
+++ frontend/src/app/component/chat-room/chat-room.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, ViewChild } from '@angular/core';
 import { ChatMessage } from "../../model/chat-message";
 import { MessageComponent } from "../message/message.component";
 import { NgClass, NgForOf, NgIf } from "@angular/common";
@@ -10,24 +10,28 @@ import { ChatRoom } from "../../model/chat-room";
 import { ChatRoomSettingsComponent } from "../chat-room-settings/chat-room-settings.component";
 import { MatIcon } from "@angular/material/icon";
 import { MatIconButton } from "@angular/material/button";
+import { debounceTime, fromEvent } from "rxjs";
+import { ChatService } from "../../service/chat.service";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-chat-room',
   standalone: true,
-	imports: [
-		MessageComponent,
-		NgForOf,
-		FormsModule,
-		NgIf,
-		NgClass,
-		ChatRoomSettingsComponent,
-		MatIcon,
-		MatIconButton
-	],
+  imports: [
+    MessageComponent,
+    NgForOf,
+    FormsModule,
+    NgIf,
+    NgClass,
+    ChatRoomSettingsComponent,
+    MatIcon,
+    MatIconButton,
+    MatProgressSpinner
+  ],
   templateUrl: './chat-room.component.html',
   styleUrl: './chat-room.component.scss'
 })
-export class ChatRoomComponent {
+export class ChatRoomComponent implements AfterViewInit {
 
   protected currentChatRoom: ChatRoom | undefined;
   private currentSubscription: StompSubscription | undefined;
@@ -35,47 +39,106 @@ export class ChatRoomComponent {
   protected message: string = '';
   protected chatRoomName: string = '';
   protected roomSettingVisible: boolean = false;
+  protected oldMessagesLoading: boolean = false;
+  protected noMoreOldMessages: boolean = false;
+
+  @ViewChild('messagesContainer', { static: true }) messagesContainer!: ElementRef;
 
   constructor(
     private websocketService: WebSocketService,
-    private dataStoreService: DataStoreService
+    private dataStoreService: DataStoreService,
+    private chatService: ChatService
   ) {
     effect(() => {
       if (!this.websocketService.isConnected()) return
 
-      if (this.currentSubscription != undefined) {
-        this.currentSubscription.unsubscribe();
-        this.chatMessages = [];
-      }
-
       this.currentChatRoom = this.dataStoreService.getCurrentlySelectedChatRoom();
-      if (this.currentChatRoom != undefined) {
-        this.chatRoomName = this.currentChatRoom.name;
-        this.currentSubscription = this.websocketService.subscribeToRoom(
-          this.currentChatRoom.id!!,
-          (message: ChatMessage[] | ChatMessage) => {
-            if (Array.isArray(message)) {
-              this.chatMessages.unshift(...message);
-            } else {
-              this.chatMessages.unshift(message);
-            }
-          }
-        )
-      }
+      this.reloadCurrentChatRoom();
     })
   }
 
-  sendMessage() {
+  ngAfterViewInit() {
+    this.initLazyLoadingOfOldMessages();
+  }
+
+  private reloadCurrentChatRoom() {
+    if (this.currentSubscription != undefined) {
+      this.currentSubscription.unsubscribe();
+    }
+    this.chatMessages = [];
+    this.oldMessagesLoading = false;
+    this.noMoreOldMessages = false;
+    if (this.currentChatRoom != undefined) {
+      this.chatRoomName = this.currentChatRoom.name;
+      this.subscribeToMessagesTopic();
+    }
+  }
+
+  private subscribeToMessagesTopic() {
+    this.currentSubscription = this.websocketService.subscribeToRoom(
+      // @ts-ignore
+      this.currentChatRoom.id,
+      (message: ChatMessage[] | ChatMessage) => {
+        if (Array.isArray(message)) {
+          this.chatMessages.unshift(...message);
+        } else {
+          this.chatMessages.unshift(message);
+        }
+      }
+    )
+  }
+
+  protected sendMessage() {
     if(this.message == "" || this.message == " ") return;
     this.websocketService.sendMessage(this.currentChatRoom!!, this.message);
     this.message = '';
   }
 
-  toggleRoomSettings() {
+  protected toggleRoomSettings() {
     this.roomSettingVisible = !this.roomSettingVisible;
   }
 
-  updateChatRoomName(newName: string) {
+  protected updateChatRoomName(newName: string) {
     this.chatRoomName = newName;
+  }
+
+  private initLazyLoadingOfOldMessages() {
+    fromEvent(this.messagesContainer?.nativeElement, 'scroll')
+      .pipe(debounceTime(100))
+      .subscribe((e: any) => {
+      if (this.shouldLoadOldMessages(e)) {
+        this.callApiToGetOldMessages();
+      }
+    });
+  }
+
+  private shouldLoadOldMessages(scrollEvent: any): boolean {
+    let scrollTop = scrollEvent?.target['scrollTop'];
+    let scrollHeight = scrollEvent?.target['scrollHeight'];
+    let offsetHeight = scrollEvent?.target['offsetHeight'];
+    const LOAD_OLD_MESSAGES_SCROLL_TRIGGER_OFFSET = 5;
+    return !this.oldMessagesLoading &&
+      !this.noMoreOldMessages &&
+      (scrollHeight + scrollTop - offsetHeight) < LOAD_OLD_MESSAGES_SCROLL_TRIGGER_OFFSET &&
+      this.chatMessages.length >= 50
+  }
+
+  private callApiToGetOldMessages() {
+    this.oldMessagesLoading = true;
+    this.chatService.loadOldMessagesForRoom(
+      this.currentChatRoom!!.id,
+      this.chatMessages.at(-1)!!.id!!
+    ).subscribe({
+      next: (messages: ChatMessage[]) => {
+        if (messages.length == 0) {
+          this.noMoreOldMessages = true;
+        }
+        this.chatMessages.push(...messages);
+        this.oldMessagesLoading = false;
+      },
+      error: (error) => {
+        this.oldMessagesLoading = false;
+      }
+    });
   }
 }
