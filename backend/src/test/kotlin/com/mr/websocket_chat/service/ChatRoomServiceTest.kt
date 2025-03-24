d15d28d1f2c69f2e83d7b7c6f677777e4dc6b07d
Mateusz Rosiak <rosa100@op.pl>
2025-03-24 21:42:57 +0100

    [#26] MockK library added. Implement actual tests for ChatRoomService

--- backend/src/test/kotlin/com/mr/websocket_chat/service/ChatRoomServiceTest.kt
+++ backend/src/test/kotlin/com/mr/websocket_chat/service/ChatRoomServiceTest.kt
@@ -1,43 +1,94 @@
 package com.mr.websocket_chat.service
 
+import com.mr.websocket_chat.domain.jpa.ChatRoomEntity
+import com.mr.websocket_chat.domain.jpa.UserEntity
+import com.mr.websocket_chat.repository.ChatRoomRepository
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
+import org.springframework.data.repository.findByIdOrNull
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ChatRoomServiceTest {
 
+	@MockK
+	private val chatRoomRepository: ChatRoomRepository = mockk()
+
+	@InjectMockKs
+	private val chatRoomService: ChatRoomService = ChatRoomService(chatRoomRepository)
+
+	private lateinit var chatRoomEntity: ChatRoomEntity
+
+	private lateinit var user1: UserEntity
+
+	private lateinit var user2: UserEntity
+
 	@BeforeEach
 	fun setUp() {
+		user1 = UserEntity(
+			username = "username1",
+			firstName = "firstName1",
+			lastName = "lastName1",
+		)
+		user2 = UserEntity(
+			username = "username2",
+			firstName = "firstName2",
+			lastName = "lastName2",
+		)
+		chatRoomEntity = ChatRoomEntity(
+			id = 0L,
+			name = "Room 1",
+			users = mutableSetOf(user1)
+		)
 	}
 
 	@Test
-	fun addChatRoom() {
+	fun whenRemovingLastRoomUser_thenDeleteRoom() {
+		// given
+		every { chatRoomRepository.findByIdOrNull(chatRoomEntity.id) } returns chatRoomEntity
+        every { chatRoomRepository.deleteById(chatRoomEntity.id) } returns Unit
+
+        // when
+		val result = chatRoomService.removeUserFromRoom(user1.username, chatRoomEntity.id)
+
+        // then
+        assertEquals(true, result)
+		verify { chatRoomRepository.deleteById(chatRoomEntity.id) }
+		verify(inverse = true) { chatRoomRepository.save(chatRoomEntity) }
 	}
 
 	@Test
-	fun findById() {
+	fun whenRemovingRoomUser_thenUpdateRoom() {
+		// given
+		chatRoomEntity.users.add(user2)
+		every { chatRoomRepository.findByIdOrNull(chatRoomEntity.id) } returns chatRoomEntity
+		every { chatRoomRepository.save(chatRoomEntity) } returns chatRoomEntity
+		every { chatRoomRepository.deleteById(chatRoomEntity.id) } returns Unit
+
+		// when
+		val result = chatRoomService.removeUserFromRoom(user1.username, chatRoomEntity.id)
+
+		// then
+		assertEquals(true, result)
+		verify { chatRoomRepository.save(chatRoomEntity) }
+		verify(inverse = true) { chatRoomRepository.deleteById(chatRoomEntity.id) }
 	}
 
 	@Test
-	fun getChatRoomsForUser() {
-	}
+	fun whenRemovingUserFromNonExistingRoom_thenDoNothing() {
+		// given
+		every { chatRoomRepository.findByIdOrNull(chatRoomEntity.id) } returns null
 
-	@Test
-	fun removeUserFromRoom() {
-	}
+		// when
+		val result = chatRoomService.removeUserFromRoom(user1.username, chatRoomEntity.id)
 
-	@Test
-	fun chatRoomExists() {
-	}
-
-	@Test
-	fun deleteChatRoom() {
-	}
-
-	@Test
-	fun isUserChatRoomMember() {
-	}
-
-	@Test
-	fun testIsUserChatRoomMember() {
+		// then
+		assertEquals(false, result)
+		verify(inverse = true) { chatRoomRepository.save(chatRoomEntity) }
+		verify(inverse = true) { chatRoomRepository.deleteById(chatRoomEntity.id) }
 	}
 }
